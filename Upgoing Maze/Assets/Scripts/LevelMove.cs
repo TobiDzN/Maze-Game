7189c8c118c1e5f22de6f8c25b1381b95d41be91
TobiDzN <reemaroes111@gmail.com>
2024-04-13 01:33:54 +0300

    .

--- Upgoing Maze/Assets/Scripts/LevelMove.cs
+++ Upgoing Maze/Assets/Scripts/LevelMove.cs
@@ -5,15 +5,28 @@ using UnityEngine;
 public class LevelMove : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    float speed = -4;
     void Start()
     {
         
     }
 
+    IEnumerator waiter ()
+    {
+        yield return new WaitForSeconds(1);
+    }
+
+
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(0, -4, 0) * Time.deltaTime; 
+        transform.position += new Vector3(0, speed, 0) * Time.deltaTime; 
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            speed = speed * 2;
+
+        }
     }
 
 
